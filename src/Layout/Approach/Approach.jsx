d409f27befe2303f7898c1a5c49a6468d4016e34
Skyzofrndev <dirgahardekaagustiantara@gmail.com>
2025-12-10 01:49:30 +0700

    Approach

---
+++ src/Layout/Approach/Approach.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Approach = () => {
+  return (
+    <div className="">
+      <p className="font-semibold text-default">Pendekatan kami pada klien</p>
+    </div>
+  );
+};
+
+export default Approach;
--- src/Layout/Index.js
+++ src/Layout/Index.js
@@ -1,3 +1,4 @@
 export { default as Hero } from './Herosection/Hero'
 export { default as Stats } from './CountStats/Stats'
 export { default as About } from './About/About'
+export { default as Approach } from './Approach/Approach'
